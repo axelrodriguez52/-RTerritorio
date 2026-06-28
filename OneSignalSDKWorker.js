e9ad65b2d07eaf520eea869f43889974a294df60
axelrodriguez52 <axelrodriguez52@gmail.com>
2026-06-28 13:23:07 -0400

    feat: OneSignal push notifications

---
+++ OneSignalSDKWorker.js
@@ -0,0 +1,58 @@
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+
+var CACHE_NAME = "rterritorio-v11";
+var ASSETS = [
+  "./",
+  "./index.html",
+  "./css/styles.css",
+  "./js/app.js",
+  "./js/export.js",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(name) {
+          return name !== CACHE_NAME;
+        }).map(function(name) {
+          return caches.delete(name);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("message", function(event) {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener("fetch", function(event) {
+  if (event.request.url.includes("script.google.com")) return;
+
+  event.respondWith(
+    fetch(event.request).then(function(response) {
+      var clone = response.clone();
+      caches.open(CACHE_NAME).then(function(cache) {
+        cache.put(event.request, clone);
+      });
+      return response;
+    }).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
